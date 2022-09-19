5924cce55b6e61ac3b7640e4620794996d117738
reawakened222 <54442871+reawakened222@users.noreply.github.com>
2022-09-19 22:16:22 +0200

    Refactored discount calculation by temporarily combining multiple if-clauses

--- csharp/SupermarketReceipt/ShoppingCart.cs
+++ csharp/SupermarketReceipt/ShoppingCart.cs
@@ -57,6 +57,11 @@ namespace SupermarketReceipt
                 if (offer.OfferType == SpecialOfferType.ThreeForTwo)
                 {
                     xForAmountDiscount = 3;
+                    if (quantityAsInt > 2)
+                    {
+                        var discountAmount = quantity * unitPrice - (quantityAsInt / xForAmountDiscount * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                        discount = new Discount(aProduct, "3 for 2", -discountAmount);
+                    }
                 }
                 else if (offer.OfferType == SpecialOfferType.TwoForAmount)
                 {
@@ -68,21 +73,17 @@ namespace SupermarketReceipt
                         discount = new Discount(aProduct, "2 for " + offer.Argument, -discountN);
                     }
                 }
-                if (offer.OfferType == SpecialOfferType.FiveForAmount) xForAmountDiscount = 5;
-
-                if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+                if (offer.OfferType == SpecialOfferType.FiveForAmount)
                 {
-                    var discountAmount = quantity * unitPrice - (quantityAsInt / xForAmountDiscount * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                    discount = new Discount(aProduct, "3 for 2", -discountAmount);
+                    xForAmountDiscount = 5;
+                    if (quantityAsInt >= 5)
+                    {
+                        var discountTotal = unitPrice * quantity - (offer.Argument * (quantityAsInt / xForAmountDiscount) + quantityAsInt % 5 * unitPrice);
+                        discount = new Discount(aProduct, xForAmountDiscount + " for " + offer.Argument, -discountTotal);
+                    }
                 }
-
                 if (offer.OfferType == SpecialOfferType.TenPercentDiscount)
                     discount = new Discount(aProduct, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
-                if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
-                {
-                    var discountTotal = unitPrice * quantity - (offer.Argument * (quantityAsInt / xForAmountDiscount) + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(aProduct, xForAmountDiscount + " for " + offer.Argument, -discountTotal);
-                }
 
                 return discount;
             }
