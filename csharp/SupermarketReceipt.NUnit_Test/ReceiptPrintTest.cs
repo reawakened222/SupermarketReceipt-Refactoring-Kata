102a76b868935d96b7de7d1f7124fe8f6f637e25
reawakened222 <54442871+reawakened222@users.noreply.github.com>
2022-09-20 03:56:18 +0200

    Started implementing feature for printing HTML receipt

---
+++ csharp/SupermarketReceipt.NUnit_Test/ReceiptPrintTest.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.Collections.Generic;
+
+namespace SupermarketReceipt.NUnit_Test
+{
+    [TestFixture]
+    public class ReceiptPrintTest
+    {
+        [TestCase]
+        public void PrintEmptyReceipt()
+        {
+            FakeCatalog catalog = new FakeCatalog();
+            Teller teller = new Teller(catalog);
+
+            ShoppingCart cart = new ShoppingCart();
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+            ReceiptPrinter printer = new ReceiptPrinter();
+            string res = printer.PrintReceipt(receipt);
+            Assert.That(res.Trim(), Is.EqualTo("Total:                              0.00"));
+
+            res = printer.PrintReceiptToHTML(receipt);
+            Assert.That(res.Trim(), 
+                Is.EqualTo(
+                    "<tr><th>Item</th><th>Total Price</th><th>Price x Amount</th></tr>"+
+                    "<tr><th>Description</th><th>Discount Amount</th></tr>"+
+                    "<tr><td>Total:</td><td>0.00</td></tr>"));
+        }
+        [TestCase]
+        public void PrintSingleItem()
+        {
+            FakeCatalog catalog = new FakeCatalog();
+            Product apple = new Product("Apple", ProductUnit.Kilo);
+            catalog.AddProduct(apple, 2.0);
+            Teller teller = new Teller(catalog);
+
+            ShoppingCart cart = new ShoppingCart();
+            cart.AddItemQuantity(apple, 1.0);
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+            ReceiptPrinter printer = new ReceiptPrinter();
+            string res = printer.PrintReceipt(receipt);
+            Assert.That(res.Trim(), Is.EqualTo("Apple                               2.00\n\n" + "Total:                              2.00"));
+
+            res = printer.PrintReceiptToHTML(receipt);
+            Assert.That(res.Trim(), Is.EqualTo(
+                    "<tr><th>Item</th><th>Total Price</th><th>Price x Amount</th></tr>" + 
+                    "<tr><td>Apple</td><td>2.00</td></tr>" +
+                    "<tr><th>Description</th><th>Discount Amount</th></tr>"+
+                    "<tr><td>Total:</td><td>2.00</td></tr>"));
+        }
+        [TestCase]
+        public void PrintSingleItemWithDiscount()
+        {
+            FakeCatalog catalog = new FakeCatalog();
+            Product apple = new Product("Apple", ProductUnit.Kilo);
+            catalog.AddProduct(apple, 2.0);
+            Teller teller = new Teller(catalog);
+            teller.AddSpecialOffer(SpecialOfferType.PercentageDiscount, apple, 10.0);
+
+            ShoppingCart cart = new ShoppingCart();
+            cart.AddItemQuantity(apple, 1.0);
+            Receipt receipt = teller.ChecksOutArticlesFrom(cart);
+            ReceiptPrinter printer = new ReceiptPrinter();
+            string res = printer.PrintReceipt(receipt);
+            Assert.That(res.Trim(), Is.EqualTo("Apple                               2.00\n10% off(Apple)                     -0.20\n\n" + "Total:                              1.80"));
+
+            res = printer.PrintReceiptToHTML(receipt);
+            Assert.That(res.Trim(), 
+                Is.EqualTo(
+                    "<tr><th>Item</th><th>Total Price</th><th>Price x Amount</th></tr>" +
+                    "<tr><td>Apple</td><td>2.00</td></tr>" +
+                    "<tr><th>Description</th><th>Discount Amount</th></tr>" +
+                    "<tr><td>10% off(Apple)</td><td>-0.20</td></tr>" +
+                    "<tr><td>Total:</td><td>1.80</td></tr>"));
+        }
+    }
+}
--- csharp/SupermarketReceipt.NUnit_Test/ReceiptPrinter.cs
+++ csharp/SupermarketReceipt.NUnit_Test/ReceiptPrinter.cs
@@ -18,35 +18,60 @@ namespace SupermarketReceipt
         public ReceiptPrinter() : this(40)
         {
         }
-
+        public delegate string ReceiptItemPrinter(ReceiptItem item);
+        public delegate string DiscountItemPrinter(Discount item);
+        public delegate string TotalItemPrinter(Receipt item);
+        public delegate string HeaderPrinter();
         public string PrintReceipt(Receipt receipt)
         {
+            return this.PrintReceipt(receipt, PrintReceiptItem, PrintDiscount, PrintTotal, () => "", () => "");
+        }
+
+        public string PrintReceiptToHTML(Receipt receipt)
+        {
+            HeaderPrinter receiptHeader = () => $"<tr><th>Item</th><th>Total Price</th><th>Price x Amount</th></tr>";
+            HeaderPrinter discountHeader = () => $"<tr><th>Description</th><th>Discount Amount</th></tr>";
+            return this.PrintReceipt(receipt, PrintReceiptItemAsHTML, PrintDiscountAsHTML, PrintTotalAsHTML, receiptHeader, discountHeader).ReplaceLineEndings("");
+        }
+        public string PrintReceipt(Receipt receipt, ReceiptItemPrinter receiptItemPrinter, DiscountItemPrinter discountItemPrinter, TotalItemPrinter totalPrinter, HeaderPrinter PrintReceiptHeader, HeaderPrinter PrintDiscountHeader)
+        {
             var result = new StringBuilder();
+            string receiptHeader = PrintReceiptHeader();
+            result.Append(receiptHeader);
             foreach (var item in receipt.GetItems())
             {
-                string receiptItem = PrintReceiptItem(item);
-                result.Append(receiptItem);
-                
+                string receiptItem = receiptItemPrinter(item);
+                result.Append(receiptItem);                
             }
 
+            string discountHeader = PrintDiscountHeader();
+            result.Append(discountHeader);
             foreach (var discount in receipt.GetDiscounts())
             {
-                string discountPresentation = PrintDiscount(discount);
+                string discountPresentation = discountItemPrinter(discount);
                 result.Append(discountPresentation);
             }
 
             {
                 result.Append("\n");
-                result.Append(PrintTotal(receipt));
+                result.Append(totalPrinter(receipt));
             }
             return result.ToString();
         }
 
         private string PrintTotal(Receipt receipt)
         {
-            string name = "Total: ";
+            string name = "Total:";
             string value = PrintPrice(receipt.GetTotalPrice());
             return FormatLineWithWhitespace(name, value);
+        }
+
+        private string PrintTotalAsHTML(Receipt receipt)
+        {
+            string name = "Total:";
+            string value = PrintPrice(receipt.GetTotalPrice());
+            string line = $"<tr><td>{name}</td><td>{value}</td></tr>";
+            return line;
         }
 
         private string PrintDiscount(Discount discount)
@@ -55,6 +80,15 @@ namespace SupermarketReceipt
             string value = PrintPrice(discount.DiscountAmount);
 
             return FormatLineWithWhitespace(name, value);
+        }
+
+        private string PrintDiscountAsHTML(Discount discount)
+        {
+            string name = discount.Description + "(" + discount.Product.Name + ")";
+            string value = PrintPrice(discount.DiscountAmount);
+
+            string line = $"<tr><td>{name}</td><td>{value}</td></tr>";
+            return line;
         }
 
         private string PrintReceiptItem(ReceiptItem item)
@@ -69,7 +103,20 @@ namespace SupermarketReceipt
 
             return line;
         }
-        
+        private string PrintReceiptItemAsHTML(ReceiptItem item)
+        {
+            string totalPrice = PrintPrice(item.TotalPrice);
+            string name = item.Product.Name;
+
+            string line = $"<tr><td>{name}</td><td>{totalPrice}</td>";
+            if (item.Quantity != 1)
+            {
+                line += "<td>" + PrintPrice(item.Price) + " * " + PrintQuantity(item) + "</td>";
+            }
+            line += "</tr>";
+            return line;
+        }
+
 
         private string FormatLineWithWhitespace(string name, string value)
         {
